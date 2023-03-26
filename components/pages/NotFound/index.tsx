3b8a7708dc4dc8c92e9528d9064f7082ca8dd22a
Belal Ahmad <belalahmad.fleapo@gmail.com>
2023-03-26 17:35:29 +0530

    fixing css

--- components/pages/NotFound/index.tsx
+++ components/pages/NotFound/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../../../styles/Home.module.css";
+import styles from "../../../styles/home.module.scss";
 
 const Index: React.FC = () => {
   return (
